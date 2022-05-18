6d4135254236190d15e8465134e866abff66354f
Christy John <christyjohn.crz@gmail.com>
2022-05-18 14:36:18 +0530

    Create index page

---
+++ src/main/java/me/christyjohn/springrecipeapp/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package me.christyjohn.springrecipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping({"", "/", "index"})
+	public String getIndexPage() {
+		return "index";
+	}
+}
